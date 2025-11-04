b7c250355f5245cf551af5167e7b7dd7b2aa4320
Name <example@gmail.com>
2025-11-04 16:37:47 +0700

    feat: cors turned off

--- src/server.js
+++ src/server.js
@@ -23,13 +23,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(helmet()); // Безопасность
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://mrkiro52.github.io'
-  ],
-  credentials: true
-}));
+app.use(cors());
 app.use(morgan('combined')); // Логирование
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
